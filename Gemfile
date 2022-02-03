fc9ec3349c7cee3fec9077a544b4a03cf89cdbc3
Zita Mello <zitamello@gmail.com>
2022-02-03 11:19:58 -0300

    fixing up deleting records

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem "jbuilder"
 
 gem "jsbundling-rails"
 
+gem 'esbuild-rails'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get 'about', to: 'pages#about'
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
