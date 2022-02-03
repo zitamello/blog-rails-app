e4c56ac3e7641c6b2514c59b43b3904915eceeb9
Zita Mello <zitamello@gmail.com>
2022-02-03 14:41:12 -0300

    adding production group to gemfile

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   #expects a home.html.erb template+
   def home
   end
+  def index
+  end
 
-end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "pages#home"
+  root "pages#index"
   get 'about', to: 'pages#about'
   # Defines the root path route ("/")
   # root "articles#index"
